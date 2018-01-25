e412c89969dd7f3a4304fe484133d4c994564e47
Ashenafi Mulat <emailmulat@gmail.com>
2018-01-25 10:15:00 -0500

    "Added Accept Number from User"

--- src/main/java/com/ashu/demo/MainController.java
+++ src/main/java/com/ashu/demo/MainController.java
@@ -3,13 +3,18 @@ package com.ashu.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Scanner;
+
 @RestController
 public class MainController {
 
     @RequestMapping("/fizzbuzz")
     public String showFizzBuzz(){
         String result = "";
-        for (int i = 1; i <=100; i ++){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int number = keyboard.nextInt();
+        for (int i = 1; i <=number; i ++){
             if(i%3==0 && i%5==0){
                 result += "FizzBuzz"+"<br />";
             }else if (i%3==0){
